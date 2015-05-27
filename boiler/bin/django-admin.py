6fba2ac2dc6ae916c944ff3c195eaf5c643795b3
Chris Hackett <chackett87@gmail.com>
2015-05-27 10:05:06 -0700

    angular boiler start

--- boiler/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/RandomRain/coding/boiler_plate/django-drf-starter-project/boiler/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
